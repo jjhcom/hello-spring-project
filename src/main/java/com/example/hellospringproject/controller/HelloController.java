a747dbd1d77b16c32580e54e67ff33ffe4fbef90
jjhcom <91904494+jjhcom@users.noreply.github.com>
2024-01-09 15:01:34 +0900

    mvc

--- src/main/java/com/example/hellospringproject/controller/HelloController.java
+++ src/main/java/com/example/hellospringproject/controller/HelloController.java
@@ -3,13 +3,20 @@ package com.example.hellospringproject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
     @GetMapping("hello")
     public String hello(Model model){
-        model.addAttribute("data", "hello!!!!");
+        model.addAttribute("data", "someone");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-mvc";
     }
 
 }
