5acbb6a7725865069d85ed342034b930d28b1d5a
jjhcom <91904494+jjhcom@users.noreply.github.com>
2024-01-09 15:21:17 +0900

    api

--- src/main/java/com/example/hellospringproject/controller/HelloController.java
+++ src/main/java/com/example/hellospringproject/controller/HelloController.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Random;
 
 @Controller
 public class HelloController {
     @GetMapping("hello")
     public String hello(Model model){
-        model.addAttribute("data", "someone");
+        model.addAttribute("data", "guest");
         return "hello";
     }
 
@@ -19,4 +22,42 @@ public class HelloController {
         return "hello-mvc";
     }
 
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello  " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        hello.setAge();
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        private String age;
+
+        public String getAge() {
+            return age;
+        }
+
+        public void setAge() {
+            Random random = new Random();
+            int ages = random.nextInt(100);
+            this.age = Integer.toString(ages);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
